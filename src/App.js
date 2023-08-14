4ae515c9a0cf2b6435525f5850984838a1cc04b9
NozomiKataoka <nk100729@icloud.com>
2023-08-14 14:13:25 +0900

    npm再インストール

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          なんだか疲れちゃった  c
+          なんだか疲れちゃった
         </p>
         <a
           className="App-link"
